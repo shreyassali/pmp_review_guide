6d869292b06bf16aa5dc6e7fe460b04a0aff0d2c
shreyassali <salishreyas@gmail.com>
2017-11-25 18:28:08 -0800

    Fixed UI

--- components/ChapterCard.js
+++ components/ChapterCard.js
@@ -1,34 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
-import { Card, CardItem, Body } from 'native-base';
+import { StyleSheet } from 'react-native';
+import { Card, CardItem, Body, Text } from 'native-base';
 
 export default class ChapterCard extends React.Component {
 
-  _onPress = () => {
-    this.props.onPressItem(this.props.id);
-  };
-
-  render() {
-      console.log('Rendering Chapter Card ' + this.props.name);
-      return(
-        <Card>
-          <CardItem button onPress={this._onPress}>
-            <Body style={styles.containerStyle}>
-              <Text style={styles.title}> {this.props.name} </Text>
-            </Body>
-          </CardItem>
-        </Card>);
+render(props){
+  return (
+      <Card style={styles.containerStyle}>
+        <CardItem>
+          <Body style={styles.bodyStyle}>
+            <Text style={styles.title}> {this.props.name} </Text>
+          </Body>
+        </CardItem>
+      </Card>
+    );
   }
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create ({
   containerStyle: {
-      marginLeft: 10,
-      marginRight: 10,
       marginTop: 7,
       marginBottom: 7,
+      marginLeft: 10,
+      marginRight: 10,
       paddingHorizontal: 12,
       paddingVertical: 10,
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  bodyStyle: {
       alignItems: 'center',
       justifyContent: 'center',
   },
