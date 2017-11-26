72679ebe149b4225933a9fc6f260c6d9bd161db0
shreyassali <salishreyas@gmail.com>
2017-11-25 16:08:02 -0800

    Updated Topics List with firebase

--- components/TopicList.js
+++ components/TopicList.js
@@ -4,6 +4,39 @@ import { List, ListItem, Text, Icon } from 'native-base';
 import Banner from './components/Banner';
 
 class TopicList extends React.Component {
+  state = {
+    isLoadingComplete: false,
+    TopicList: null,
+  };
+
+  static navigationOptions = ({ navigation, screenProps }) => ({
+
+  });
+
+  componentDidMount() {
+    if(this.state.isLoadingComplete) {
+      return;
+    }
+    //Read from firebase
+    var topicList = [];
+    firebase.database().ref('chapterDetails').once('value').then(function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        var chapter = childSnapshot.val();
+        chapter.id = childSnapshot.key;
+        if (this.props.id == chapter.id) {
+          console.log(chapter);
+          topicList.push(chapter.topics);
+        }
+      });
+      return topicList;
+    }).then((topicList) => this.setState({isLoadingComplete: true,
+                  topicList: topicList}));
+  }
+
+  _onPress = () => {
+    console.log("Shreyas Hello");
+  };
+  
 render(){
   <List>
     <ListItem button onPress={() => _handleOnPress(item)}>
