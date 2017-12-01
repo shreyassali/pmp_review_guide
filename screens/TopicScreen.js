355988d73798e7a6edb86f037b2473b31a81ee63
shreyassali <salishreyas@gmail.com>
2017-11-30 20:56:35 -0800

    Implemented Navigation

--- screens/TopicScreen.js
+++ screens/TopicScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import firebase from 'firebase';
-import { View, Button } from 'react-native';
+import { Platform, StyleSheet, TouchableOpacity, View, FlatList, Button } from 'react-native';
 import { List, ListItem, Text, Icon } from 'native-base';
 import Banner from '../components/Banner';
 
@@ -8,42 +8,57 @@ class TopicScreen extends React.Component {
   state = {
     isLoadingComplete: false,
     chapter: null,
+    chapterName: null,
   };
 
   static navigationOptions = ({ navigation }) => ({
+    //title: this.state.chapterName,
+    headerLeft: <Button title="Go Back" onPress={() => navigation.goBack()}/>
   });
 
   componentDidMount() {
     if(this.state.isLoadingComplete) {
       return;
     }
+    const {state} = this.props.navigation;
+    var chapterDetailsDB = 'chapterDetails/' + state.params.chapterId;
 
-    //Read from firebase
-    firebase.database().ref('chapterDetails/' + '0').once('value').then(function(snapshot) {
-        var chapter = snapshot.val();
-        console.log(chapter);
-        return chapter;
-      }).then((chapter) => this.setState({isLoadingComplete: true,
-                  chapter: chapter}));
+    firebase.database().ref(chapterDetailsDB).once('value').then(function(snapshot) {
+        var chapterDetails = snapshot.val();
+        return chapterDetails;
+      }).then((chapterDetails) => this.setState(
+        { isLoadingComplete: true,
+          chapter: chapterDetails.topics,
+          chapterName: chapterDetails.name
+        }));
   }
 
-  _onTopicPress = () => {
-    console.log("Shreyas Hello");
-  };
+  _renderItem = ({item}) => (
+    <List>
+        <ListItem button onPress={() => _handleOnPress(item)}>
+            <Text>{item.topic_name}</Text>
+        </ListItem>
+   </List>
+  );
+
+  _keyExtractor = (item, index) => item.topic_id;
 
   render() {
+    const {state} = this.props.navigation;
+    console.log(this.state.chapterName);
     return (
       <View style={{ flex: 1, backgroundColor: 'white' }} contentInsetAdjustmentBehavior="automatic">
-        <Banner headerText={this.state.chapter.name}/>
-        <List dataArray={this.state.chapter.topics}
-              renderRow={(item) =>
-              <ListItem button onPress={() => _onTopicPress(item)}>
-                <Text>{item}</Text>
-              </ListItem>}>
-        </List>
+        <FlatList
+          data={this.state.chapter}
+          keyExtractor={this._keyExtractor}
+          renderItem={this._renderItem}/>
       </View>
     );
   }
 }
 
-export default () => <TopicScreen />;
+_handleOnPress = (item) => {
+  console.log(item.name);
+};
+
+export default TopicScreen;
