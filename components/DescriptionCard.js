0790caa0fcb53141bf89cc4666724c1dde171d4a
shreyassali <salishreyas@gmail.com>
2017-12-01 09:19:01 -0800

    Added componenet to screen

--- components/DescriptionCard.js
+++ components/DescriptionCard.js
@@ -1,19 +1,57 @@
-import React, { Component } from 'react';
-import { Container, Content, Card, CardItem, Text, Thumbnail } from 'native-base';
-​
-export default class DescriptionCard extends Component {
+import React from 'react';
+import { Image, View, StyleSheet } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Left, Body, Button } from 'native-base';
+import { Entypo } from '@expo/vector-icons';
+
+export default class DescriptionCard extends React.Component {
     render() {
         return (
-            <Container>
-                <Content>
-                    <Card>
-                        <CardItem button onPress={function_call()}>
-                            // <Thumbnail source={require('./img/NSP.png')} />
-                            <Text>Native Starter Pro</Text>
-                        </CardItem>
-                   </Card>
-                </Content>
-            </Container>
+          <Container>
+            <Content>
+              <Card style={styles.containerStyle}>
+                <CardItem>
+                  <Left>
+                    <Thumbnail source={{uri: 'Image URL'}} />
+                    <Body>
+                      <Text>NativeBase</Text>
+                      <Text note>April 15, 2016</Text>
+                    </Body>
+                  </Left>
+                </CardItem>
+                <CardItem>
+                  <Body>
+                    <Image source={{uri: 'Image URL'}} style={{height: 200, width: 200, flex: 1}}/>
+                    <Text>
+                      Sample Test Topic Description
+                    </Text>
+                  </Body>
+                </CardItem>
+                <CardItem>
+                  <Left>
+                    <Button transparent textStyle={{color: '#87838B'}}>
+                      <Entypo name="github" />
+                      <Text>1,926 stars</Text>
+                    </Button>
+                  </Left>
+                </CardItem>
+              </Card>
+            </Content>
+          </Container>
         );
     }
 }
+
+
+const styles = StyleSheet.create ({
+  containerStyle: {
+      marginTop: 12,
+      marginBottom: 7,
+      marginLeft: 10,
+      marginRight: 10,
+      paddingHorizontal: 12,
+      paddingVertical: 10,
+      alignItems: 'center',
+      justifyContent: 'center',
+      flex: 0,
+  },
+});
--- screens/TopicDescriptionScreen.js
+++ screens/TopicDescriptionScreen.js
@@ -2,46 +2,13 @@ import React from 'react';
 import { Image, View } from 'react-native';
 import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Left, Body, Button } from 'native-base';
 import { Entypo } from '@expo/vector-icons';
+import DescriptionCard from '../components/DescriptionCard';
 
 class TopicDescriptionScreen extends React.Component {
 
-  static navigationOptions = ({ navigation }) => ({
-    headerLeft: <Button title="Go Back" onPress={() => navigation.goBack('Topics')}/>
-  });
-
   render() {
     return (
-      <Container>
-        <Content>
-          <Card style={{flex: 0}}>
-            <CardItem>
-              <Left>
-                <Thumbnail source={{uri: 'Image URL'}} />
-                <Body>
-                  <Text>NativeBase</Text>
-                  <Text note>April 15, 2016</Text>
-                </Body>
-              </Left>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Image source={{uri: 'Image URL'}} style={{height: 200, width: 200, flex: 1}}/>
-                <Text>
-                  Sample Test Topic Description
-                </Text>
-              </Body>
-            </CardItem>
-            <CardItem>
-              <Left>
-                <Button transparent textStyle={{color: '#87838B'}}>
-                  <Entypo name="github" />
-                  <Text>1,926 stars</Text>
-                </Button>
-              </Left>
-            </CardItem>
-          </Card>
-        </Content>
-      </Container>
+      <DescriptionCard />
     );
   }
 }
